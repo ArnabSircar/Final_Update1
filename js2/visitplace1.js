d8a2811ae9b641d2eacb37460ba968f20ef91638
Arnab Sircar <arnab.mist.cse@gmail.com>
2022-01-15 01:10:37 +0600

    update2

--- js2/visitplace1.js
+++ js2/visitplace1.js
@@ -2,7 +2,7 @@
 
 function myFun() {
 //    location.replace("bus.html",'_blank')
-if(document.getElementById("empty")=="Click this select menu")
+// if(document.getElementById("empty")=="Click this select menu")
    window.open("bus.html", '_blank');
     //document.location='bus.html'
 }
